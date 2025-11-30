144e668e8ef34372d15597426907db45599da1d0
swavi89 <swaviman89@gmail.com>
2025-11-30 22:28:46 +0530

    convert direct from ajax load of all data & change data attribute to value when delete

--- resources/views/citizens/index.blade.php
+++ resources/views/citizens/index.blade.php
@@ -14,8 +14,8 @@
     <div class="float-end mb-2 me-3">
         <a href="{{ url('citizens/create') }}" class="btn btn-primary">Add Citizen</a>
     </div>
-    <div>
-        <table class="table table-responsive table-striped mt-5">
+    <div class="container">
+        <table class="table table-responsive table-hover mt-5">
             <thead>
                 <tr>
                     <th>ID</th>
@@ -31,80 +31,51 @@
                 </tr>
             </thead>
             <tbody id="citizen_list"></tbody>
+            @foreach($citizens as $c)
+            <tr>
+                <td>{{ $c->id }}</td>
+                <td>{{ $c->citizen_name }}</td>
+                <td>{{ $c->village->panchayat->block->state->state_name }}</td>
+                <td>{{ $c->village->panchayat->block->block_name }}</td>
+                <td>{{ $c->village->panchayat->panchayat_name }}</td>
+                <td>{{ $c->village->village_name }}</td>
+                <td>{{ $c->gender }}</td>
+                <td>{{ $c->citizen_phone }}</td>
+                <td>{{ $c->citizen_email }}</td>
 
+                <td><a href="{{ url('citizens/' . $c->id . '/edit') }}"><i class="fas fa-edit"></i></a></td>
+                <td><a role="button" class="delete-btn" value="{{ $c->id }}"><i class="fas fa-trash text-danger"></i></a></td>
+            </tr>
+            @endforeach
             </tbody>
         </table>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script>
-$(document).ready(function () {
+        $(document).ready(function() {
+            // Delete citizen
+            $(document).on("click", ".delete-btn", function() {
+                let id = $(this).attr("value");
 
-    // Load all citizens
-    function loadCitizens() {
+                if (!confirm("Are you sure you want to delete this citizen?")) {
+                    return;
+                }
 
-        $.get("/citizens-list", function (response) {
-
-            let rows = "";
-
-            response.forEach(c => {
-                rows += `
-                    <tr>
-                        <td>${c.id}</td>
-                        <td>${c.citizen_name}</td>
-                        <td>${c.village.panchayat.block.state.state_name}</td>
-                        <td>${c.village.panchayat.block.block_name}</td>
-                        <td>${c.village.panchayat.panchayat_name}</td>
-                        <td>${c.village.village_name}</td>
-                        <td>${c.gender}</td>
-                        <td>${c.citizen_phone}</td>
-                        <td>${c.citizen_email}</td>
-
-                        <td>
-                            <a href="/citizens/${c.id}/edit" class="btn btn-sm btn-warning">
-                                <i class="fa fa-edit"></i>
-                            </a>
-                        </td>
-
-                        <td>
-                            <button class="btn btn-sm btn-danger delete-btn" data-id="${c.id}">
-                                <i class="fa fa-trash"></i>
-                            </button>
-                        </td>
-                    </tr>
-                `;
+                $.ajax({
+                    url: `/citizens/${id}`,
+                    type: "DELETE",
+                    data: {
+                        _token: "{{ csrf_token() }}"
+                    },
+                    success: function(res) {
+                        alert("Citizen deleted successfully!");
+                        window.location.href = "/citizens";
+                    }
+                });
             });
-
-            $("#citizen_list").html(rows);
         });
-    }
-
-    // Initial load
-    loadCitizens();
-
-    // Delete citizen
-    $(document).on("click", ".delete-btn", function () {
-        let id = $(this).data("id");
-
-        if (!confirm("Are you sure you want to delete this citizen?")) {
-            return;
-        }
-
-        $.ajax({
-            url: `/citizens/${id}`,
-            type: "DELETE",
-            data: {
-                _token: "{{ csrf_token() }}"
-            },
-            success: function (res) {
-                alert("Citizen deleted successfully!");
-                loadCitizens();
-            }
-        });
-    });
-
-});
-</script>
+    </script>
 
 </body>
 
